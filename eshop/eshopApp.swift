5674890d8521e6937474df761c39b513c78d0949
prince24999 <phucdoankaizen@gmail.com>
2024-07-16 11:24:50 +0700

    Initial Commit

---
+++ eshop/eshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  eshopApp.swift
+//  eshop
+//
+//  Created by vp on 16/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct eshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
